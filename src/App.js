e145740e9175b535fb959ef78062b37a1ca3e357
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-06 19:08:40 -0600

    Header y menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Typography, Menu } from 'antd';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
+const { SubMenu } = Menu;
 
 function App() {
   return (
     <div>
      <Layout>
-      <Header>Header</Header>
+      <Header style={{padding:15}}>
+        <Avatar style={{float:'right'}} icon={<UserOutlined />} />
+        <Title style={{color:'white'}}level={3}>Armando</Title>
+      </Header>
       <Layout>
-        <Sider style={{background:'red'}}>Sider</Sider>
+        <Sider>
+          <Menu theme="dark" mode="inline">
+            <Menu.Item key="1">Siniestros</Menu.Item>
+            <Menu.Item key="2">Clientes corporativos</Menu.Item>
+            <Menu.Item key="3">Finanzas</Menu.Item>
+            <Menu.Item key="4">Historial</Menu.Item>
+            <SubMenu key="sub1" title="Exportar">
+              <Menu.Item key="5">PDF</Menu.Item>
+              <Menu.Item key="6">Excel</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
         <Layout>
           <Content>Content</Content>
           <Footer>Footer</Footer>
